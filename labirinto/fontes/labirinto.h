d9fbd62b9e13f4059805af19a8ac9ff52e024666
lorenzosaraiva <lorenzo.saraiva@hotmail.com>
2014-10-27 23:59:16 -0200

    Update labirinto.h
    Documentação da função de inserir nó

--- labirinto/fontes/labirinto.h
+++ labirinto/fontes/labirinto.h
@@ -1,24 +1,24 @@
 /***************************************************************************
 *
-* $MCD MÛdulo de definiÁ„o: LAB LABIRINTO em Grafo  
+* $MCD M√≥dulo de defini√ß√£o: LAB LABIRINTO em Grafo  
 *
 * Arquivo gerado: labirinto.h
 * Letras identificadoras: LAB
 *
-* Nome da base de software: ArcabouÁo para a automaÁ„o de testes de programas redigidos em C
+* Nome da base de software: Arcabou√ßo para a automa√ß√£o de testes de programas redigidos em C
 *
 * Projeto: INF 1301 Trabalho 3
 * Gestor: LES/DI/PUC-Rio
 * Autores:  Lucas Menezes <LM>, Lorenzo Saraiva <LS>
 *
-* $HA HistÛrico de evoluÁ„o:
-* Vers„o Autor Data ObservaÁıes
-* 1     LM,LS 24/out/2014 InÌcio do Desenvolvimento
+* $HA Hist√≥rico de evolu√ß√£o:
+* Vers√£o Autor Data Observa√ß√µes
+* 1     LM,LS 24/out/2014 In√≠cio do Desenvolvimento
 
-* $ED DescriÁ„o do mÛdulo
+* $ED Descri√ß√£o do m√≥dulo
 * 
-*	Este mÛdulo implementa um Labirinto usando o um grafo genÈrico
-*	Este mÛdulo armazena em si 
+*	Este m√≥dulo implementa um Labirinto usando o um grafo gen√©rico
+*	Este m√≥dulo armazena em si 
 * 
 
 ***************************************************************************/
@@ -29,29 +29,29 @@ typedef enum {
          	LAB_CondRetOK ,
                /* Concluiu corretamente */
 		 LAB_CondRetFaltouMemoria ,
-		      /* N„o h· memÛria pra alocar */
+		      /* N√£o h√° mem√≥ria pra alocar */
         	 LAB_CondRetLabirintoVazio ,
-               /* o grafo n„o contÈm elementos */
+               /* o grafo n√£o cont√©m elementos */
 		 
 
    } LAB_tpCondRet ;
 /***********************************************************************
 *
-*  $FC FunÁ„o: LAB  &Criar Labirinto
+*  $FC Fun√ß√£o: LAB  &Criar Labirinto
 *
-*  $ED DescriÁ„o da funÁ„o
-*     Cria a estrutura b·sica do Labirinto Vazio
-*     Os possÌveis tipos de elementos armazenados nos nÛs do grafo s„o desconhecidos a priori
-*     Esta funÁ„o inicializa as estruturas reguladoras do grafo
+*  $ED Descri√ß√£o da fun√ß√£o
+*     Cria a estrutura b√°sica do Labirinto Vazio
+*     Os poss√≠veis tipos de elementos armazenados nos n√≥s do grafo s√£o desconhecidos a priori
+*     Esta fun√ß√£o inicializa as estruturas reguladoras do grafo
 *
-*  $EP Par‚metros
-*	  refLab		- endereÁo ponteiro para a cabeÁa do labirinto a ser gerado, a ser retornado por referÍncia
+*  $EP Par√¢metros
+*	  refLab		- endere√ßo ponteiro para a cabe√ßa do labirinto a ser gerado, a ser retornado por refer√™ncia
 *                     
 *
 *  $FV Valor retornado
-*     Se executou corretamente retornar· a LAB_CondRetOK
-*     Se ocorreu alguma alocaÁ„o de memÛria n„o conseguir ser realizada
-*     a funÁ„o retornar· LAB_CondRetFaltouMemoria.
+*     Se executou corretamente retornar√° a LAB_CondRetOK
+*     Se ocorreu alguma aloca√ß√£o de mem√≥ria n√£o conseguir ser realizada
+*     a fun√ß√£o retornar√° LAB_CondRetFaltouMemoria.
 *    
 *
 ***********************************************************************/
@@ -59,16 +59,16 @@ LAB_tpCondRet LAB_CriarLabirinto( LAB_tppLabirinto* refLab);
 
 /***********************************************************************
 *
-*  $FC FunÁ„o: GRA  &Destruir Labirinto
+*  $FC Fun√ß√£o: GRA  &Destruir Labirinto
 *
-*  $ED DescriÁ„o da funÁ„o
-*     Esvazia o Labirinto recebido e destrÛi suas estruturas de controle.
+*  $ED Descri√ß√£o da fun√ß√£o
+*     Esvazia o Labirinto recebido e destr√≥i suas estruturas de controle.
 *
-*  $EP Par‚metros
-*	  labirinto			- ponteiro para a cabeÁa do labirinto a ser destruido
+*  $EP Par√¢metros
+*	  labirinto			- ponteiro para a cabe√ßa do labirinto a ser destruido
 *
 *  $FV Valor retornado
-*     Se executou corretamente retorna retornar· a LAB_CondRetOK
+*     Se executou corretamente retorna retornar√° a LAB_CondRetOK
 *    
 *    
 *
@@ -76,3 +76,25 @@ LAB_tpCondRet LAB_CriarLabirinto( LAB_tppLabirinto* refLab);
 
 
 LAB_tpCondRet LAB_DestruirLabirinto( LAB_tppLabirinto labirinto );
+
+/***********************************************************************
+*
+*  $FC Fun√ß√£o: LAB  &Inserir Posicao Norte
+*
+*  $ED Descri√ß√£o da fun√ß√£o
+*     Insere uma posi√ß√£o jogavel  ao norte da corrente
+*     Caso seja a primeira posi√ß√£o a ser inserida, se torna a corrente
+*
+*  $EP Par√¢metros
+*	   labirinto			- ponteiro para a cabe√ßa do labirinto a inserir a posi√ß√£o
+*                     
+*
+*  $FV Valor retornado
+*     Se executou corretamente retornar√° a LAB_CondRetOK
+*     Se ocorreu alguma aloca√ß√£o de mem√≥ria n√£o conseguir ser realizada
+*     a fun√ß√£o retornar√° LAB_CondRetFaltouMemoria.
+* 
+*
+***********************************************************************/
+
+LAB_tpCondRet LAB_InserirPosicaoNorte ( LAB_tppLabirinto labirinto );
