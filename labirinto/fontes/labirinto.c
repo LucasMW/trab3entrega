18a57d32ac60c6d76e4b201a89ae94ebcb3f6c3c
lorenzosaraiva <lorenzo.saraiva@hotmail.com>
2014-10-29 19:28:25 -0200

    Update geral
    Finalização da InserirPosicaoNorte e criação das análogas, criação da IrPosicaoNorte e de suas análogas

--- labirinto/fontes/labirinto.c
+++ labirinto/fontes/labirinto.c
@@ -137,28 +137,215 @@ LAB_tpCondRet LAB_DestruirLabirinto( LAB_tppLabirinto labirinto)
 *  Função: LAB &Inserir Posicao Norte
 *  ****/
 
-LAB_tpCondRet LAB_InserirPosicaoNorte ( LAB_tppLabirinto labirinto ,  PosJog_Tipo tipo)
+LAB_tpCondRet LAB_InserirPosicaoNorte ( LAB_tppLabirinto labirinto, PosJog_Tipo tipo )
 {
-	/* Insere uma Posição ao Norte da Corrente */
-	int temp;
-	LAB_PosJog posicao;
-	posicao=(LAB_PosJog)malloc(sizeof(struct LAB_PosicaoJogavel));
-	posicao->tipo = tipo;
-	posicao->idNorte=0;
-	posicao->idOeste=0;
-	posicao->idLeste=0;
-	posicao->idSul= labirinto->idCorrente;
+	LAB_PosJog posicaoNova, posicaoOrigem;
+	posicaoNova = (LAB_PosJog)malloc(sizeof(LAB_PosJog));
+	posicaoNova->tipo = tipo;
+	posicaoNova->idLeste = 0;
+	posicaoNova->idOeste = 0;
+	posicaoNova->idNorte = 0;
+	posicaoNova->idSul = labirinto->idCorrente;
 	if (labirinto->idCorrente = 0){
-		if( GRA_InserirNo( labirinto->pGrafo,posicao,&labirinto->idCorrente ) != GRA_CondRetOK)
+		if( GRA_InserirNo( labirinto->pGrafo,posicaoNova,&labirinto->idCorrente ) != GRA_CondRetOK)
 			return LAB_CondRetFaltouMemoria;
 		return LAB_CondRetOK;
 	}
-	temp = labirinto->idCorrente;
-	if (GRA_InserirNo( labirinto->pGrafo,posicao,&labirinto->idCorrente ) != GRA_CondRetOK)
-		return LAB_CondRetOK;
-	if (GRA_InserirAresta(labirinto->pGrafo,temp,labirinto->idCorrente, 'V')!= GRA_CondRetOK)
+	
+	GRA_ObterValorNo(labirinto->pGrafo, labirinto->idCorrente,(void**)&posicaoOrigem);
+	if (posicaoOrigem->idNorte != 0){
+		free(posicaoNova);
+		return LAB_tpCondRetJaExistePosicao;	
+	}
+	if (GRA_InserirNo( labirinto->pGrafo,posicaoNova,&labirinto->idCorrente ) != GRA_CondRetOK)
+		return LAB_CondRetFaltouMemoria;
+	if (GRA_InserirAresta(labirinto->pGrafo,labirinto->idCorrente,posicaoNova->idSul, 0)!= GRA_CondRetOK)
 		return LAB_CondRetFaltouMemoria; /* Aqui deve ser checada a condret retornada para ver corretamente o erro */
+	posicaoOrigem->idNorte = labirinto->idCorrente;
 	return LAB_CondRetOK;
 }
 
 /* Fim função: LAB &Inserir Posicao Norte */
+
+/***************************************************************************
+*
+*  Função: LAB &Inserir Posicao Sul
+*  ****/
+
+LAB_tpCondRet LAB_InserirPosicaoSul ( LAB_tppLabirinto labirinto, PosJog_Tipo tipo )
+{
+	LAB_PosJog posicaoNova, posicaoOrigem;
+	posicaoNova = (LAB_PosJog)malloc(sizeof(LAB_PosJog));
+	posicaoNova->tipo = tipo;
+	posicaoNova->idLeste = 0;
+	posicaoNova->idOeste = 0;
+	posicaoNova->idNorte = labirinto->idCorrente;
+	posicaoNova->idSul = 0;
+	if (labirinto->idCorrente = 0){
+		if( GRA_InserirNo( labirinto->pGrafo,posicaoNova,&labirinto->idCorrente ) != GRA_CondRetOK)
+			return LAB_CondRetFaltouMemoria;
+		return LAB_CondRetOK;
+	}
+	
+	GRA_ObterValorNo(labirinto->pGrafo, labirinto->idCorrente,(void**)&posicaoOrigem);
+	if (posicaoOrigem->idSul != 0){
+		free(posicaoNova);
+		return LAB_tpCondRetJaExistePosicao;	
+	}
+	if (GRA_InserirNo( labirinto->pGrafo,posicaoNova,&labirinto->idCorrente ) != GRA_CondRetOK)
+		return LAB_CondRetFaltouMemoria;
+	if (GRA_InserirAresta(labirinto->pGrafo,labirinto->idCorrente,posicaoNova->idNorte, 0)!= GRA_CondRetOK)
+		return LAB_CondRetFaltouMemoria; /* Aqui deve ser checada a condret retornada para ver corretamente o erro */
+	posicaoOrigem->idSul = labirinto->idCorrente;
+	return LAB_CondRetOK;
+}
+
+/* Fim função: LAB &Inserir Posicao Sul */
+
+
+/***************************************************************************
+*
+*  Função: LAB &Inserir Posicao Oeste
+*  ****/
+
+LAB_tpCondRet LAB_InserirPosicaoOeste ( LAB_tppLabirinto labirinto, PosJog_Tipo tipo )
+{
+	LAB_PosJog posicaoNova, posicaoOrigem;
+	posicaoNova = (LAB_PosJog)malloc(sizeof(LAB_PosJog));
+	posicaoNova->tipo = tipo;
+	posicaoNova->idLeste = labirinto->idCorrente;
+	posicaoNova->idOeste = 0;
+	posicaoNova->idNorte = 0;
+	posicaoNova->idSul = 0;
+	if (labirinto->idCorrente = 0){
+		if( GRA_InserirNo( labirinto->pGrafo,posicaoNova,&labirinto->idCorrente ) != GRA_CondRetOK)
+			return LAB_CondRetFaltouMemoria;
+		return LAB_CondRetOK;
+	}
+	
+	GRA_ObterValorNo(labirinto->pGrafo, labirinto->idCorrente,(void**)&posicaoOrigem);
+	if (posicaoOrigem->idOeste != 0){
+		free(posicaoNova);
+		return LAB_tpCondRetJaExistePosicao;	
+	}
+	if (GRA_InserirNo( labirinto->pGrafo,posicaoNova,&labirinto->idCorrente ) != GRA_CondRetOK)
+		return LAB_CondRetFaltouMemoria;
+	if (GRA_InserirAresta(labirinto->pGrafo,labirinto->idCorrente,posicaoNova->idLeste, 0)!= GRA_CondRetOK)
+		return LAB_CondRetFaltouMemoria; /* Aqui deve ser checada a condret retornada para ver corretamente o erro */
+	posicaoOrigem->idOeste = labirinto->idCorrente;
+	return LAB_CondRetOK;
+}
+
+/* Fim função: LAB &Inserir Posicao Oeste */
+
+/***************************************************************************
+*
+*  Função: LAB &Inserir Posicao Leste
+*  ****/
+
+LAB_tpCondRet LAB_InserirPosicaoLeste ( LAB_tppLabirinto labirinto, PosJog_Tipo tipo )
+{
+	LAB_PosJog posicaoNova, posicaoOrigem;
+	posicaoNova = (LAB_PosJog)malloc(sizeof(LAB_PosJog));
+	posicaoNova->tipo = tipo;
+	posicaoNova->idLeste = 0;
+	posicaoNova->idOeste = labirinto->idCorrente;
+	posicaoNova->idNorte = 0;
+	posicaoNova->idSul = 0;
+	if (labirinto->idCorrente = 0){
+		if( GRA_InserirNo( labirinto->pGrafo,posicaoNova,&labirinto->idCorrente ) != GRA_CondRetOK)
+			return LAB_CondRetFaltouMemoria;
+		return LAB_CondRetOK;
+	}
+	
+	GRA_ObterValorNo(labirinto->pGrafo, labirinto->idCorrente,(void**)&posicaoOrigem);
+	if (posicaoOrigem->idLeste != 0){
+		free(posicaoNova);
+		return LAB_tpCondRetJaExistePosicao;	
+	}
+	if (GRA_InserirNo( labirinto->pGrafo,posicaoNova,&labirinto->idCorrente ) != GRA_CondRetOK)
+		return LAB_CondRetFaltouMemoria;
+	if (GRA_InserirAresta(labirinto->pGrafo,labirinto->idCorrente,posicaoNova->idOeste, 0)!= GRA_CondRetOK)
+		return LAB_CondRetFaltouMemoria; /* Aqui deve ser checada a condret retornada para ver corretamente o erro */
+	posicaoOrigem->idOeste = labirinto->idCorrente;
+	return LAB_CondRetOK;
+}
+
+/* Fim função: LAB &Inserir Posicao Oeste */
+
+/***************************************************************************
+*
+*  Função: LAB &Ir Posicao Norte
+*  ****/
+
+LAB_tpCondRet IrPosicaoNorte ( LAB_tppLabirinto labirinto ){
+
+	LAB_PosJog posicaoCorrente;
+	if (GRA_ObterValorNo(labirinto->pGrafo,labirinto->idCorrente,(void**)&posicaoCorrente)!= GRA_CondRetOK)
+		return LAB_CondRetLabirintoVazio;
+	if (posicaoCorrente->idNorte == 0)
+		return LAB_CondRetNaoExisteCaminho;
+	labirinto->idCorrente = posicaoCorrente->idNorte;
+	return LAB_CondRetOK;
+	
+}
+
+/* Fim função: LAB &Ir Posicao Norte */
+
+/***************************************************************************
+*
+*  Função: LAB &Ir Posicao Sul
+*  ****/
+
+LAB_tpCondRet IrPosicaoSul ( LAB_tppLabirinto labirinto ){
+
+	LAB_PosJog posicaoCorrente;
+	if (GRA_ObterValorNo(labirinto->pGrafo,labirinto->idCorrente,(void**)&posicaoCorrente)!= GRA_CondRetOK)
+		return LAB_CondRetLabirintoVazio;
+	if (posicaoCorrente->idSul == 0)
+		return LAB_CondRetNaoExisteCaminho;
+	labirinto->idCorrente = posicaoCorrente->idSul;
+	return LAB_CondRetOK;
+	
+}
+
+/* Fim função: LAB &Ir Posicao Sul */
+
+/***************************************************************************
+*
+*  Função: LAB &Ir Posicao Oeste 
+*  ****/
+
+LAB_tpCondRet IrPosicaoOeste ( LAB_tppLabirinto labirinto ){
+
+	LAB_PosJog posicaoCorrente;
+	if (GRA_ObterValorNo(labirinto->pGrafo,labirinto->idCorrente,(void**)&posicaoCorrente)!= GRA_CondRetOK)
+		return LAB_CondRetLabirintoVazio;
+	if (posicaoCorrente->idOeste  == 0)
+		return LAB_CondRetNaoExisteCaminho;
+	labirinto->idCorrente = posicaoCorrente->idOeste ;
+	return LAB_CondRetOK;
+	
+}
+
+/* Fim função: LAB &Ir Posicao Oeste */
+
+/***************************************************************************
+*
+*  Função: LAB &Ir Posicao Leste
+*  ****/
+
+LAB_tpCondRet IrPosicaoLeste ( LAB_tppLabirinto labirinto ){
+
+	LAB_PosJog posicaoCorrente;
+	if (GRA_ObterValorNo(labirinto->pGrafo,labirinto->idCorrente,(void**)&posicaoCorrente)!= GRA_CondRetOK)
+		return LAB_CondRetLabirintoVazio;
+	if (posicaoCorrente->idLeste == 0)
+		return LAB_CondRetNaoExisteCaminho;
+	labirinto->idCorrente = posicaoCorrente->idLeste;
+	return LAB_CondRetOK;
+	
+}
+
+/* Fim função: LAB &Ir Posicao Leste */
+
